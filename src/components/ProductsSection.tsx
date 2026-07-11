860c84bee472db4c6b1c8e7aa7654c57b454c025
Avantika Bhadke <avantikabhadke@gmail.com>
2026-07-11 20:26:22 +0530

    Responsiveness

--- src/components/ProductsSection.tsx
+++ src/components/ProductsSection.tsx
@@ -66,7 +66,7 @@ const ProductsSection = () => {
   ];
 
   return (
-    <section className="py-20 bg-background">
+    <section className="py-12 bg-background">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Section Header */}
         <div className="text-center mb-16">
